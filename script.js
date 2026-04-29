3fd407310f9b9d473fac1b97fb8eebb719cde2b7
imam-programmer <imamprogrammer09@gmail.com>
2026-04-29 19:59:03 +0600

    interactive navbar

---
+++ script.js
@@ -0,0 +1,13 @@
+let one=document.querySelector('#one');
+let two=document.querySelector('#two');
+let about=document.querySelector('#about');
+let service=document.querySelector('#service');
+one.addEventListener('click',()=>{
+    about.style.display='block'
+     service.style.display='none'
+})
+two.addEventListener('click',()=>{
+    service.style.display='block';
+    
+    about.style.display='none'
+})
